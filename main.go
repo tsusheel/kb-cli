08acf29eb99b7f608a76249e16717e2e81a4cc72
Sushil Thakur <tsusheel.135@gmail.com>
2026-03-21 17:24:08 -0400

    restructure

--- main.go
+++ main.go
@@ -1,25 +1,8 @@
-package cmd
+package main 
 
-import (
-	"fmt"
-	"os"
+import "github.com/tsusheel/kb-cli/cmd"
 
-	"github.com/spf13/cobra"
-)
-
-var rootCmd = &cobra.Command{
-	Use:   "kb",
-	Short: "Knowledge base CLI",
-	Long:  "A CLI tool to manage your personal knowledge base.",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Welcome to kb-cli")
-	},
-}
-
-func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+func main() {
+	cmd.Execute()
 }
 
