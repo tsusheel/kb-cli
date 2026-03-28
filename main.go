3a99313858340c081c0a3409841a9bd1637f8ffd
Sushil Thakur <tsusheel.135@gmail.com>
2026-03-28 17:08:10 -0400

    updates

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/tsusheel/kb-cli/cmd"
 	"github.com/tsusheel/kb-cli/db"
@@ -10,16 +12,42 @@ import (
 )
 
 func main() {
-	viper.SetConfigName("config")
-	viper.AddConfigPath("$HOME/.config/kb")
+	home, _ := os.UserHomeDir()
+	configDir := filepath.Join(home, ".config", "kb")
+	configFile := filepath.Join(configDir, "config.yaml")
+
+	viper.SetConfigFile(configFile)
 	viper.SetConfigType("yaml")
 
-	app.InitApp()
-
+	// Try reading config
 	err := viper.ReadInConfig()
 	if err != nil {
-		fmt.Println("No config found")
+		fmt.Println("Config not found, creating default config...")
+
+		// 1. Create directory if not exists
+		if err := os.MkdirAll(configDir, os.ModePerm); err != nil {
+			panic(err)
+		}
+
+		// 2. Set default values
+		viper.Set("app_name", "kb-app")
+
+		// 3. Create the config file
+		file, err := os.Create(configFile)
+		if err != nil {
+			panic(err)
+		}
+		file.Close()
+
+		// 4. Write defaults to file
+		if err := viper.WriteConfigAs(configFile); err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Default config created at:", configFile)
 	}
+
+	app.InitApp()
 
 	cmd.Execute()
 }
